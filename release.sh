7c962d04984509e9fd4940416d17794f62705bba
Jarppe <jarppe@metosin.fi>
2018-02-03 18:27:40 +0200

    release.sh script

---
+++ release.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env zsh
+
+set -e
+
+message=${1:-Another magnificent release}
+
+function die {
+  echo "error" $1
+  exit 1
+}
+
+git diff-index --quiet HEAD -- || die "git not clean"
+
+echo "releasing" $message
+
+rm -fr ./docs/*
+cp -r ./resources/public/index.html ./docs/
+cp -r ./resources/public/lib ./docs/
+lein dist
+#git add ./docs
+#git commit -m "$message"
+#git push
+
+echo "Done"
